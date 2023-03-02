8cd23c5a0cdd54317e01a4be0786a55b5e2c11c0
YOUSUF <yufis0844@gmail.com>
2023-03-02 19:05:07 +0300

    test

--- config/conn.php
+++ config/conn.php
@@ -1,3 +1,11 @@
 <?php
-$conn = "name"
+
+$conn = new mysqli("localhost", "root", "", "somali-blood-donation-system");
+
+if($conn->connect_error){
+    echo $conn->error;
+}else{
+    echo "connect succesfully";
+}
+
 ?>
